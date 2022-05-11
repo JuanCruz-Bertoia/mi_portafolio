292116300fc003534672b8cd0171ca6c862eb340
Juan C. Bertoia <bertoia114@gmail.com>
2022-05-11 14:27:04 -0300

    desploy portafolio

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\View\Components\home\Portafolio;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Blade::component('portafolio', Portafolio::class);
+
         if (config('app.env') === 'production') {
             URL::forcedScheme('https');
         }
