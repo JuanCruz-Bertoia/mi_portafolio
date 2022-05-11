eef37ca2767be5898d91a4a1c16e8899d39a2c11
Juan C. Bertoia <bertoia114@gmail.com>
2022-05-11 14:59:38 -0300

    desploy portafolio

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-
-Route::post('/contact/submit', [\App\Http\Controllers\ContactController::class, 'submit']);
