ba8183a6fc4a367258ae61e817a88ce3a989616b
Juan C. Bertoia <bertoia114@gmail.com>
2022-05-11 11:17:12 -0300

    desploy portafolio

--- resources/views/components/home/portafolio.blade.php
+++ resources/views/components/home/portafolio.blade.php
@@ -14,6 +14,29 @@
                 </div>
             </div>
         </div>
-
+        <div class="flex flex-wrap justify-center -mx-4">
+            <div class="w-full px-4">
+                <ul class="flex flex-wrap justify-center mb-12 space-x-1">
+                    <li class="mb-1">
+                        <button @click="selectedTab = 'all' " :class="selectedTab == 'all' ? activeClasses : inactiveClasses " class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition ">
+                            Todos mis projectos
+                        </button>
+                    </li>
+                    @foreach($tabs as $tab)
+                    <li class="mb-1">
+                        <button @click="selectedTab = '{{$tab}}' " :class="selectedTab === '{{$tab}}' ? activeClasses : inactiveClasses " class="inline-block py-2 md:py-3 px-5 lg:px-8 rounded-lg text-base font-semibold text-center transition ">
+                            {{ $tab }}
+                        </button>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        <div class="flex flex-wrap -mx-4">
+            @foreach ($items as $item)
+            <x-portafolio-item :title="$item['title']" :categories="$item['category']" :image="$item['image']" :description="$item['description']" :github="$item['github']"></x-portafolio-item>
+            @endforeach
+        </div>
+    </div>
 </section>
 <!-- ====== Portafolio Section End -->
