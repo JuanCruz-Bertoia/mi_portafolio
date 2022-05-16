6dbb0da481c138a0e1b43b2cd07659db0e2ff1d1
Juan C. Bertoia <bertoia114@gmail.com>
2022-05-16 10:53:43 -0300

    desploy portafolio

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Portafolio Juan C. Bertoia</title>
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
